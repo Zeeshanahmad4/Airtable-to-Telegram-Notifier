5bc99b3ed6ba8e1639b49431f16c3eebeacc65d6
Zeeshan Ahmad <ahmadzee26@gmail.com>
2023-09-12 13:41:04 +0500

    Update telegram_api.py

--- modules/telegram/telegram_api.py
+++ modules/telegram/telegram_api.py
@@ -1,1 +1,21 @@
+# telegram_api.py
+
+import requests
+
+BOT_TOKEN = "YOUR_TELEGRAM_BOT_TOKEN"
+CHAT_ID = "YOUR_TELEGRAM_CHAT_ID"
+
+TELEGRAM_SEND_MESSAGE_ENDPOINT = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
+
+def send_message_to_telegram(text):
+    """
+    Send a message to the Telegram channel.
+    """
+    payload = {
+        "chat_id": CHAT_ID,
+        "text": text,
+        "parse_mode": "Markdown"
+    }
+    response = requests.post(TELEGRAM_SEND_MESSAGE_ENDPOINT, data=payload)
+    return response.json()
  
